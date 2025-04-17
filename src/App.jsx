08fcc21f0eeb3a54cc556fa01eac89f34ab1a921
BrentonMallory <bcm00011@mix.wvu.edu>
2025-04-17 14:08:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,14 +35,6 @@ function App() {
         </header>
 
         <main>
-          <div className="dropdown">
-            <button className="dropbtn">Dropdown</button>
-            <div className="dropdown-content">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
-            </div>
-          </div>
           <Routes>
             <Route
               path="/"
@@ -51,17 +43,16 @@ function App() {
                   <h2>Welcome to Roy's!</h2>
                   <p>
                     Since our founding, we have been the place to find good stuff at prices much steeper than most!
-                  </p>  
-                  <div class="dropdown">
-                    <button class="dropbtn">Categories</button>
-                    <div class="dropdown-content">
+                  </p>
+                  <div className="dropdown">
+                    <button className="dropbtn">Categories</button>
+                    <div className="dropdown-content">
                       <a href="#">Shoes</a>
                       <a href="#">Apparel</a>
-                      <a href="#">Equiptment</a>
+                      <a href="#">Equipment</a>
                     </div>
-                </div>
+                  </div>
                 </section>
-                
               }
             />
             <Route path="/about" element={<About />} />
